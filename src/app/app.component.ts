3f3ecc8fbc86a56fe51d7365fc607a2fb40817da
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-05-21 01:12:29 +0500

    feat(ui): add a light/dark toggle using DaisyUI
    - used platform ID to detect server and browser
    - uses the initial theme for the browser
    - stores it in the local storage (only available in browser)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'QuantifAI';
+
+  private platformID = inject(PLATFORM_ID);
+
+  isDarkMode = signal(false);
+  constructor() {
+    if (isPlatformBrowser(this.platformID)) {
+      this.isDarkMode.set(this.getInitialTheme());
+    }
+  }
+  private getInitialTheme(): boolean {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme ? savedTheme === 'coffee' : false;
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode.update((value) => {
+      const newValue = !value;
+      if (isPlatformBrowser(this.platformID)) {
+        localStorage.setItem('theme', newValue ? 'coffee' : 'caramellatte');
+      }      
+      return newValue;
+    });
+  }
 }
