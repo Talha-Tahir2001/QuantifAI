c5c741656ee84f869698830cd0a94b6a22c03f86
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-05-29 01:11:16 +0500

    refactor(ui): add social links in the footer
    - add icons for social links in the footer
    - implement dynamic time using date pipe

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { LucideAngularModule, GithubIcon, LinkedinIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [DatePipe, LucideAngularModule],
   templateUrl: './footer.component.html',
   styles: ``
 })
 export class FooterComponent {
-
+  readonly GithubIcon = GithubIcon;
+  readonly LinkedinIcon = LinkedinIcon;
+  
+  public Year : Date = new Date();
 }
