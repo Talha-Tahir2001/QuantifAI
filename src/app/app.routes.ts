745a197d11633fefdc9d71d9df76f2f1817b9f62
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-06-06 01:17:57 +0500

    refactor: fix the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),        
+    },
+    {
+        path: 'detect',
+        loadComponent: () => import('./pages/detect/detect.component').then(m => m.DetectComponent),        
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent),        
+    }
+];
