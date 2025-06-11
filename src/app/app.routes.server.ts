045847765dc3aa948da483f808cc876f756fa777
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-06-12 01:08:31 +0500

    feat: add server routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'detect',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'about',
     renderMode: RenderMode.Prerender
   }
+
+
 ];
