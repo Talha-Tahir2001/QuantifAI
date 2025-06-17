bd24b5589190c633aa0b53ab9318614af690015a
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-06-18 01:17:55 +0500

    refactor: remove unnecessary imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,13 @@
-import { Component, inject, PLATFORM_ID, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styles: [],
+  styles: [],  
 })
 export class AppComponent {
-  title = 'QuantifAI';
-
-  private platformID = inject(PLATFORM_ID);
-
-  isDarkMode = signal(false);
-  constructor() {
-    if (isPlatformBrowser(this.platformID)) {
-      this.isDarkMode.set(this.getInitialTheme());
-    }
-  }
-  private getInitialTheme(): boolean {
-    const savedTheme = localStorage.getItem('theme');
-    return savedTheme ? savedTheme === 'coffee' : false;
-  }
-
-  toggleTheme(): void {
-    this.isDarkMode.update((value) => {
-      const newValue = !value;
-      if (isPlatformBrowser(this.platformID)) {
-        localStorage.setItem('theme', newValue ? 'coffee' : 'caramellatte');
-      }      
-      return newValue;
-    });
-  }
+  title = 'QuantifAI'; 
 }
