30d8918e29a4fb231e1b0c4f651bc44dbb8616fd
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-06-20 00:05:30 +0500

    chore: add environments for dev and prod

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    GOOGLE_GEMINI_API_KEY: ''
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    GOOGLE_GEMINI_API_KEY: '',    
+};
