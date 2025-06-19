2cffe178b8077b9a36c3c08e7e10babd4acfab66
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-06-20 00:48:48 +0500

    chore: convert to ES module from CommonJs

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
-const path = require('path');
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-const serverDistPath = path.join(process.cwd(), 'dist/quantif-ai/server/server.mjs');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+const serverDistPath = join(__dirname, '../dist/quantif-ai/server/server.mjs');
+
+// Vercel expects you to export a default handler from the file
 export default import(serverDistPath).then(module => module.app);
