7760d85fdf3363432f68c093f36439285b7cdc53
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-06-19 23:59:21 +0500

    feat(ui): add an About route

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
+import {LucideAngularModule, UserIcon} from 'lucide-angular';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './about.component.html',
   styles: ``
 })
 export class AboutComponent {
-
+  readonly UserIcon = UserIcon;
 }
