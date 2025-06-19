6bd1b8b84f42ecca7a6ec520ef132b7d943e0cf7
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-06-20 00:00:33 +0500

    feat(ui): add a "how it works" section

---
+++ src/app/components/how-it-works/how-it-works.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-how-it-works',
+  imports: [],
+  templateUrl: './how-it-works.component.html',
+  styles: ``
+})
+export class HowItWorksComponent {
+
+}
